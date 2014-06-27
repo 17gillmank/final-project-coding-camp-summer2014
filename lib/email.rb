508cd29cf6e4a9ad6bcb66f6fcf517c53d190d2d
17gillmank <kaylagolfs@aol.com>
2014-06-27 13:52:38 -0400

    fix

--- lib/email.rb
+++ lib/email.rb
@@ -14,15 +14,15 @@ class Email
 
 		@mailgun = Mailgun(:api_key => 'key-57tsip-q95ud6bki67v65a1l7we40oi2')
 		
-		scraper = Scrape.new
+		@scraper = Scrape.new
 		#binding.pry
 
 		parameters = {
   			:to => "email_address",
   			:subject => "Today's Weather",
   			:text => "Good morning!
-	Today there is a #{scraper.chance_of_rain_today} chance of rain and #{scraper.weather_conditions_today}. The high is #{scraper.temperature_today}.
-  	Tonight there is a #{scraper.chance_of_rain_tonight} chance of rain and #{scraper.weather_conditions_tonight}. The low is #{scraper.temperature_tonight}. ",
+	Today there is a #{@scraper.chance_of_rain_today} chance of rain and #{@scraper.weather_conditions_today}. The high is #{@scraper.temperature_today}.
+  	Tonight there is a #{@scraper.chance_of_rain_tonight} chance of rain and #{@scraper.weather_conditions_tonight}. The low is #{@scraper.temperature_tonight}. ",
   			:from => "postmaster@sandbox1bd64ea45b5147c2894437d89ceaba68.mailgun.org"
 		}
 		parameters[:to] = recipient
